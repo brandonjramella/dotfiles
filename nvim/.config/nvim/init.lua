4d9c04bb902e2f3d58477635077f11d26a04d5b4
Brandon Ramella <bjrlabs@theramellas.cc>
2026-08-11 21:26:04 -0400

    Changed nvim background to use terminal background

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,3 +7,8 @@ vim.opt.expandtab = true
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
